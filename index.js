1cc9ceaeeda53a177f07b9f454eee059054eabf7
kylehr <kylehr@gmail.com>
2025-03-01 15:13:51 +1100

    Stringify when logging env.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function ls() {
 async function t() {
   let i = 3;
   do {
-      console.log(`test log ${new Date()} ${process.env}`);
+      console.log(`test log ${new Date()} ${JSON.stringify(process.env)}`);
       await new Promise(resolve => setTimeout(resolve, 20000)); 
       i--;
     } while (i);
