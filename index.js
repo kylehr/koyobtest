70c5720659706a4ddf5d27ece0dc07b5fcb5dbb6
kylehr <kylehr@gmail.com>
2025-03-14 12:42:40 +1100

    Add num cps to logged parameters.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let number_gamers = 20;
 let site = 'https://tableteacher.com';
 let number_sequences = process.env.STREAMS == 'CPUS' ? parseInt(numCPUs) : (process.env.STREAMS ? parseInt(process.env.STREAMS) : 1);
 let number_iterations = process.env.ITERATIONS ? parseInt(process.env.ITERATIONS) : 1;
-console.log(`number_gamers: ${number_gamers}, process.env.STREAMS: ${process.env.STREAMS}, process.env.ITERATIONS: ${process.env.ITERATIONS}, number_sequences: ${number_sequences}, number_iterations: ${number_iterations}`)
+console.log(`number_gamers: ${number_gamers}, process.env.STREAMS: ${process.env.STREAMS}, process.env.ITERATIONS: ${process.env.ITERATIONS}, number_sequences: ${number_sequences}, number_iterations: ${number_iterations}, numCPUs: ${numCPUs}`)
 
 if (cluster.isPrimary) {
   let  = log_fname = `${(new Date()).toISOString().substring(0,19).replaceAll(':', '.')}.log`;
