8c0699a699f7d4d71fcae070850bc4aa33db67c5
kylehr <kylehr@gmail.com>
2025-07-14 12:41:54 +1000

    Latest game file and results file before sub-journeys implemented.

--- rmtest.sh
+++ rmtest.sh
@@ -1,2 +1,3 @@
 # remove logs and retest
-rm *.log; SITE=https://mr-v1.fly.dev STREAMS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
+rm *.log; SITE=https://tableteacher.com GAMERS=1 STREAMS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
+#rm *.log; SITE=https://mr-v1.fly.dev STREAMS=1 GAMERS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
