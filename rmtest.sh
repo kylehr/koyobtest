a260e2786e6d054ce685b4b95c0164f56e47758f
kylehr <kylehr@gmail.com>
2025-08-01 12:08:25 +1000

    Test locally and simulate server instance id from fly.io.

--- rmtest.sh
+++ rmtest.sh
@@ -1,3 +1,4 @@
 # remove logs and retest
-rm *.log; SITE=https://tableteacher.com GAMERS=1 STREAMS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
+rm *.log; FLY_MACHINE_ID=`date +%s` SITE=http://localhost:5000 GAMERS=1 STREAMS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
+#rm *.log; SITE=https://tableteacher.com GAMERS=1 STREAMS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
 #rm *.log; SITE=https://mr-v1.fly.dev STREAMS=1 GAMERS=1 ITERATIONS=1 TIMEOUT=420000 node index.js
